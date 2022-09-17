188ec96166f43df5cb57c527ca375cef8dd0bc0e
Thiago Bezerra de Carvalho <tb161372@gmail.com>
2022-09-16 22:10:01 -0300

    musiquinha de filme

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Let's Groove");
+Console.ForegroundColor = ConsoleColor.Red;
+
+Console.WriteLine("Let this groove, get you to move,");
+Console.WriteLine("It's alright, alright");
+Console.WriteLine("Stand up, alright");
+Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.WriteLine("Gonna tell you what you can do,");
+Console.WriteLine("With my love, alright");
+Console.WriteLine("Let you know girl you're looking good");
+Console.WriteLine("You're out of sight and alright");
+Console.ForegroundColor = ConsoleColor.Gray;
+
+Console.WriteLine("Move yourself and glide like a 747");
+Console.WriteLine("Loose yourself in the sky among the clouds in the heavens");
+Console.ForegroundColor = ConsoleColor.Cyan;
+
+Console.ResetColor();
