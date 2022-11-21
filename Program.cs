1404d813881ff084e018b20ae1bcd36ff9909266
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2022-11-21 18:44:49 -0300

    acho que arrumei

--- Program.cs
+++ Program.cs
@@ -1,20 +1,25 @@
-﻿Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("Let's Groove");
-Console.ForegroundColor = ConsoleColor.Red;
+﻿Console.Clear();
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine("Let's Groove - Earth Wind & Fire");
+Console.WriteLine();
+Console.ResetColor();
 
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine("Let this groove, get you to move,");
 Console.WriteLine("It's alright, alright");
 Console.WriteLine("Stand up, alright");
-Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine();
 
+Console.ForegroundColor = ConsoleColor.Gray;
 Console.WriteLine("Gonna tell you what you can do,");
 Console.WriteLine("With my love, alright");
 Console.WriteLine("Let you know girl you're looking good");
 Console.WriteLine("You're out of sight and alright");
-Console.ForegroundColor = ConsoleColor.Gray;
+Console.WriteLine();
 
+Console.ForegroundColor = ConsoleColor.Cyan;
 Console.WriteLine("Move yourself and glide like a 747");
 Console.WriteLine("Loose yourself in the sky among the clouds in the heavens");
-Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine();
 
 Console.ResetColor();
